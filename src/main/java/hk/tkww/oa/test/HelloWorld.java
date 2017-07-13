a37c484c6b09653a9e92bd762678a30e59f9a2bf
zyw <mscboom@gmail.com>
2017-07-13 14:43:27 +0800

    add springMVC support

---
+++ src/main/java/hk/tkww/oa/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package hk.tkww.oa.test;
+
+/**
+ * Created by zhongyw on 2017/7/12.
+ */
+public class HelloWorld {
+
+    public static final void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
